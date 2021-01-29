29eb3508a4b942251bcbe070578c37288c248904
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2021-01-29 19:30:21 +0300

    fixed bug with incorrect display of the instructor's schedule header

--- client/src/pages/SettingsPage/subcomponents/InstructorSchedule/TableHead.jsx
+++ client/src/pages/SettingsPage/subcomponents/InstructorSchedule/TableHead.jsx
@@ -4,22 +4,22 @@ import { useSelector } from 'react-redux'
 
 const TableHead = ({ activeWeek, activeMode }) => {
   const { weekDayLabels } = useSelector(state => state.settings.schedule)
-
   return (
     <thead>
       <tr>
         <th className={s.cell}> </th>
         {weekDayLabels.map((day, index) => (
           <th className={s.cell} key={index}>
-            {activeWeek[0].daysInMonth() - activeWeek[0].getDate() < 0
-              ? `${activeWeek[1].getDate() + index}
-                         ${activeWeek[1]
-                           .toLocaleString('ru', { month: 'long' })
-                           .slice(0, 3)}./ `
-              : `${activeWeek[0].getDate() + index}
-                         ${activeWeek[0]
-                           .toLocaleString('ru', { month: 'long' })
-                           .slice(0, 3)}./ `}
+            {activeMode.value === 'current' &&
+              (activeWeek[0].daysInMonth() - activeWeek[0].getDate() < 0
+                ? `${activeWeek[1].getDate() + index}
+                           ${activeWeek[1]
+                             .toLocaleString('ru', { month: 'long' })
+                             .slice(0, 3)}./ `
+                : `${activeWeek[0].getDate() + index}
+                           ${activeWeek[0]
+                             .toLocaleString('ru', { month: 'long' })
+                             .slice(0, 3)}./ `)}
             {day[activeMode.value === 'periodic' ? 0 : 1]}
           </th>
         ))}
