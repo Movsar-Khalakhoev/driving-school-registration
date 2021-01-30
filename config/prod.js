871ff9963f8769d57c564fd2be11671f19aaedce
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2021-01-30 19:27:47 +0300

    preparation for uploading to the server

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  PORT: 80,
+  DB_PASSWORD: 'JGz1kISCuJSvUQHf',
+  DB_URL:
+    'mongodb+srv://Movsar:JGz1kISCuJSvUQHf@cluster0.bcfif.mongodb.net/school',
+  SECRET_KEY: 'SECRET_KEY_',
+  BASE_URL: 'http://movsar.site/',
+}
